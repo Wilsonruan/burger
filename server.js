b2441cc56c66210f3f614f6c3cdfccf2c26ab2bd
Wilson Ruan <wilsonruan23@gmail.com>
2020-08-20 20:05:28 -0400

    Base setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 var express = require("express");
+const mysql = require('mysql');
 var exphbs = require("express-handlebars");
 
 // Create an instance of the express app.
@@ -9,14 +10,35 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+//Express middlewares to handle POST request and have req.body available
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//Mysql connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: '',
+});
+
+//Connection to MySQL Server
+connection.connect((err) => {
+  if (err) throw err;
+
+  console.log('Connected to MySQL!');
+});
+
 // Routes
 app.get("/index", function(req, res) {
-    res.render("index", );
-  });
+  res.render("index", );
+});
+
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
