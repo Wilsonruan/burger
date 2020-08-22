aeb441cc667d1c9797d5746244353d048000b8a8
Wilson Ruan <wilsonruan23@gmail.com>
2020-08-22 01:34:28 -0400

    Inits seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  burger_name VARCHAR(30),
+  burger_name VARCHAR(100),
   devoured BOOLEAN DEFAULT false
 );
 SELECT * from burgers;
