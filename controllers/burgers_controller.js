a1603f60bc06b86aae7550bbf2a16ef887f80eee
Wilson Ruan <wilsonruan23@gmail.com>
2020-08-29 18:21:07 -0400

    Fixed bug

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,7 +14,7 @@ router.get("/", function (req, res) {
   
   // Create a new burger
   router.post("/api/burgers", function(req, res) {
-    burger.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
       res.json({ id: result.insertId });
     });
   });
@@ -31,8 +31,8 @@ router.get("/", function (req, res) {
   //   });
   // });
   
-  // Delete a burger
-  router.put("/burgers/:id", function(req, res) {
+  // Update a burger
+  router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
@@ -49,6 +49,19 @@ router.get("/", function (req, res) {
         res.status(200).end();
       }
     );
+  });
+
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
   });
 
   module.exports = router;
