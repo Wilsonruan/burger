8e9766da63f8e707965af33f4d345f58e731e39e
Wilson Ruan <wilsonruan23@gmail.com>
2020-08-29 22:59:57 -0400

    Able to devout it and order it again

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -32,24 +32,24 @@ router.get("/", function (req, res) {
   // });
   
   // Update a burger
-  // router.put("/api/burgers/:id", function(req, res) {
-  //   var condition = "id = " + req.params.id;
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
   
-  //   console.log("condition", condition);
+    console.log("condition", condition);
   
-  //   burger.update(
-  //     {
-  //       devoured: req.body.devoured
-  //     },
-  //     condition,
-  //     function(result) {
-  //       if (result.changedRows === 0) {
-  //         return res.status(404).end();
-  //       }
-  //       res.status(200).end();
-  //     }
-  //   );
-  // });
+    burger.update(
+      {
+        devoured: req.body.devoured
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
 
   router.delete("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
